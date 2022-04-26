e3e15d77e8a4aa647083af76543fcd15986a19ee
Jared Esparza <jared.esparza@nan-tic.com>
2022-04-26 13:16:03 +0200

    Replace test setuptools command by unittest discover

--- tests/__init__.py
+++
@@ -1,6 +0,0 @@
-# This file is part product_bulk module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
-from .test_product_bulk import suite
-
-__all__ = ['suite']
